637ae70de789d7f72b773ff2ce2323f73c11cca5
yc27-ua <yc27@alu.ua.es>
2026-02-07 18:00:57 +0100

    P01 completado: Maven, tests y ejercicio 2

--- P01-IntelliJ/simpleMavenProject/src/test/java/ppss/EjemploTest.java
+++ P01-IntelliJ/simpleMavenProject/src/test/java/ppss/EjemploTest.java
@@ -9,19 +9,40 @@ class EjemploTest {
 
     @BeforeEach
     public void setup() {
-        ejemplo= new Ejemplo();
+        ejemplo = new Ejemplo();
     }
 
     @Test
     void C1_fechaValida_should_be_true_when_date_is_29_2_2020() {
-        //Preparamos los datos (Arrange)
         int dia = 29;
         int mes = 2;
         int anyo = 2020;
         boolean resultadoEsperado = true;
-        //Ejecutamos (Act)
+
         boolean resultadoReal = ejemplo.fechaValida(dia,mes,anyo);
         assertEquals(resultadoEsperado, resultadoReal);
     }
 
+    @Test
+    void C2_fechaValida_should_be_true_when_dia_31_and_mes_8() {
+        int dia = 31;
+        int mes = 8;
+        int anyo = 2019;
+        boolean resultadoEsperado = true;
+
+        boolean resultadoReal = ejemplo.fechaValida(dia,mes,anyo);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
+
+    @Test
+    void C3_fechaValida_should_be_false_when_dia_is_negative() {
+        int dia = -2;
+        int mes = 5;
+        int anyo = 2019;
+        boolean resultadoEsperado = false;
+
+        boolean resultadoReal = ejemplo.fechaValida(dia,mes,anyo);
+        assertEquals(resultadoEsperado, resultadoReal);
+    }
 }
+
